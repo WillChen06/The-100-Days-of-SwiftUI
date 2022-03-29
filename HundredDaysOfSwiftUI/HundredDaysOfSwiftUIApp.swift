fa66559abec6ed063ddaaea6a88b49bd43d148e1
Will <will@goodarc.com>
2022-03-29 13:38:34 +0800

    First init

---
+++ HundredDaysOfSwiftUI/HundredDaysOfSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HundredDaysOfSwiftUIApp.swift
+//  HundredDaysOfSwiftUI
+//
+//  Created by WillChen on 2022/3/29.
+//
+
+import SwiftUI
+
+@main
+struct HundredDaysOfSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
