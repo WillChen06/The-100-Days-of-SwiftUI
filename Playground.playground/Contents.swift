8dfebd8169779f13797658297c9b047874ebf210
Will <will@goodarc.com>
2022-03-30 15:39:09 +0800

    Day4 - Type annotations and checkpoint 2

--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -1,92 +1,31 @@
 import Cocoa
 
-// Array
-var beatles = ["John", "Paul", "George", "Ringo"]
-beatles.append("Adrian")
-beatles.append("Allen")
+// Type Annotations
+let surname: String = "Lasso"
+var score2: Int = 0
 
-let numbers = [4, 8, 15, 16, 23, 42]
-var temperatures = [25.3, 28.2, 26.4]
+let playerName2: String = "Roy"
+var luckNumber: Int = 13
+let pi: Double = 3.141
+var isAuthenticated2: Bool = true
 
-print(beatles[0])
-print(numbers[1])
-print(temperatures[2])
 
-var scores = Array<Int>()
-scores.append(100)
-scores.append(80)
-scores.append(85)
-print(scores[1])
+var albums2: [String] = ["Red", "Fearless"]
+var user: [String: String] = ["id": "@twostraws"]
+var books: Set<String> = Set(["The Bluest Eye", "Foundation", "Girl, Woman, Other"])
 
-var albums = ["Folklore"]
-albums.append("Fearless")
-albums.append("Red")
-print(albums.count)
+var soda: [String] = ["Coke", "Pepsi", "Irn-Bru"]
+var teams: [String] = [String]()
+var cities2: [String] = []
+var clues = [String]()
 
-var characters = ["Lana", "Pam", "Ray", "Sterling"]
-print(characters.count)
-
-characters.remove(at: 2)
-print(characters.count)
-
-characters.removeAll()
-print(characters.count)
-
-let bondMovies = ["Casino Royale", "Spectre", "No Time to Die"]
-print(bondMovies.contains("Frozen"))
-
-let cities = ["London", "Tokyo", "Rome", "Budapest"]
-print(cities.sorted())
-
-let presidents = ["Bush", "Obama", "Trump", "Biden"]
-let reversedPresidents = presidents.reversed()
-print(reversedPresidents)
-
-// Dictionary
-let employee = ["name": "Taylor Swift", "job": "Singer", "location": "Nashville"]
-
-print(employee["name", default: "Unknown"])
-print(employee["job", default: "Unknown"])
-print(employee["location", default: "Unknown"])
-
-let hasGraduated = [
-    "Eric": false,
-    "Maeve": true,
-    "Otis": false
-]
-
-let olympics = [
-    2012: "London",
-    2016: "Rio de Janeiro",
-    2021: "Tokyo"
-]
-
-print(olympics[2012, default: "Unknown"])
-
-var heights = [String: Int]()
-heights["Yao Ming"] = 229
-heights["Shaquille O'Neal"] = 216
-heights["LeBron James"] = 206
-
-var archEnemies = [String: String]()
-archEnemies["Batman"] = "The Joker"
-archEnemies["Superman"] = "Lex Luthor"
-archEnemies["Batman"] = "Penguin"
-
-// Set
-var actors = Set<String>()
-actors.insert("Denzel Washington")
-actors.insert("Tom Cruise")
-actors.insert("Nicolas Cage")
-actors.insert("Samuel L Jackson")
-print(actors)
-
-// Enum
-
-enum Weekday {
-    case monday, tuesday, wednesday, thursday, friday
+enum UIStyle {
+    case light, dark, system
 }
 
-var day = Weekday.monday
-day = .tuesday
-day = .friday
+var style: UIStyle = UIStyle.light
+style = .dark
+
+let username: String
+username = "@twostraws"
+print(username)
