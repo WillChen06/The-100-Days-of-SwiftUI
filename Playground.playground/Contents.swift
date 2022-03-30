da3965025fb838616ae81753be7c2c902cc48b89
Will <will@goodarc.com>
2022-03-30 10:51:57 +0800

    Day1 - Variables, constants, strings, and numbers

--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -1,3 +1,83 @@
 import Cocoa
 
+// Variables & Constants
 var greeting = "Hello, playground"
+
+var name  = "Ted"
+name = "Rebecca"
+name = "Keeley"
+
+let character = "Daphne"
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
+
+let managerName = "Michael Scott"
+let dogBreed = "Samoyed"
+let meaningOfLife = "How many roads must a man walk down?"
+
+// Strings
+let actor = "Denzel Washington"
+let fileName = "paris.jpg"
+let result = "⭐️ You win! ⭐️"
+
+let quote = "Then he tapped a sign saying \"Believe\" and walked away."
+
+let move = """
+A day in
+the life of an
+Apple engineer
+"""
+
+let nameLength = actor.count
+print(nameLength)
+
+print(result.uppercased())
+
+print(move.hasPrefix("A day"))
+
+print(fileName.hasSuffix(".jpg"))
+
+// Numbers
+
+let score = 10
+let reallyBig = 1_00_000_000 // ignore underscores
+
+let lowerScore = score - 2
+let higherScore = score + 10
+let dounleScore = score * 2
+let squaredScore = score * score
+let halvedScore = score / 2
+
+var counter = 10
+counter += 5
+print(counter)
+
+counter *= 2
+counter -= 10
+counter /= 2
+
+let number = 120
+print(number.isMultiple(of: 3))
+print(120.isMultiple(of: 3))
+
+// Decimal numbers
+
+let num = 0.1 + 0.2
+print(num)
+
+let a = 1
+let b = 2.0
+let c = Double(a) + b
+
+let double1 = 3.1
+let double2 = 3131.3131
+let double3 = 3.0
+let int1 = 3
+
