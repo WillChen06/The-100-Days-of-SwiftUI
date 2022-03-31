cd16e92feb7c343bd87403111183c468f6bd0517
Will <will@goodarc.com>
2022-03-31 14:28:04 +0800

    Day5 - if, switch, and the ternary operator

--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -1,31 +1,153 @@
 import Cocoa
 
-// Type Annotations
-let surname: String = "Lasso"
-var score2: Int = 0
-
-let playerName2: String = "Roy"
-var luckNumber: Int = 13
-let pi: Double = 3.141
-var isAuthenticated2: Bool = true
-
-
-var albums2: [String] = ["Red", "Fearless"]
-var user: [String: String] = ["id": "@twostraws"]
-var books: Set<String> = Set(["The Bluest Eye", "Foundation", "Girl, Woman, Other"])
-
-var soda: [String] = ["Coke", "Pepsi", "Irn-Bru"]
-var teams: [String] = [String]()
-var cities2: [String] = []
-var clues = [String]()
-
-enum UIStyle {
-    case light, dark, system
+// if
+let score = 85
+if score > 80 {
+    print("Great job!")
 }
 
-var style: UIStyle = UIStyle.light
-style = .dark
+let speed = 88
+let percentage = 85
+let age = 10
+if speed >= 88 {
+    print("Where we're going, we don't need roads.")
+}
 
-let username: String
-username = "@twostraws"
-print(username)
+if percentage < 85 {
+    print("Sorry, you fail the test.")
+}
+
+if age >= 10 {
+    print("You're eligible to vote.")
+}
+
+let outName = "Dave Lister"
+let friendName = "Arnoid Rimer"
+
+if outName < friendName {
+    print("It's \(outName) vs \(friendName)")
+}
+
+if outName > friendName {
+    print("It's \(friendName) vs \(outName)")
+}
+
+var numbers = [1, 2, 3]
+numbers.append(4)
+
+if numbers.count > 3 {
+    numbers.remove(at: 0)
+}
+print(numbers)
+
+let country = "Canada"
+if country == "Australia" {
+    print("G'day!")
+}
+
+enum Sizes: Comparable {
+    case small
+    case medium
+    case large
+}
+
+let first = Sizes.small
+let second = Sizes.large
+print(first < second)
+
+// Multiple conditions
+let temp = 25
+if temp > 20 && temp < 30 {
+    print("It's a nice day.")
+}
+
+let userAge = 14
+let hasParentalConsent = true
+
+if userAge >= 10 || hasParentalConsent == true {
+    print("You can buy the game!")
+}
+
+enum TransportOption {
+    case airplane, helicopter, bicycle, car, escooter
+}
+
+let transport = TransportOption.airplane
+if transport == .airplane || transport == .helicopter {
+    print("Let's fly!")
+} else if transport == .bicycle {
+    print("I hope there's a bike path.")
+} else if transport == .car {
+    print("Time to get stuck in traffic.")
+} else {
+    print("I'm going to hire a scooter now!")
+}
+
+// Switch
+
+enum Weather {
+    case sun, rain, wind, snow, unknown
+}
+
+let forecast = Weather.sun
+
+switch forecast {
+case .sun:
+    print("It should be a nice day.")
+case .rain:
+    print("Pack an umbrella.")
+case .wind:
+    print("Wear something warm.")
+case .snow:
+    print("School is cancelled.")
+case .unknown:
+    print("Our forecast generator is broken!")
+}
+
+let place = "Metropolis"
+switch place {
+case "Gotham":
+    print("You're Baman!")
+case "Mega-City One":
+    print("You're Black Panther!")
+case "Wakanda":
+    print("You're Black Panther!")
+default:
+    print("Who are you?")
+}
+
+let day = 5
+print("My true love gava to me...")
+
+switch day {
+case 5:
+    print("5 golden rings")
+case 4:
+    print("4 calling birds")
+case 3:
+    print("3 French hens")
+case 2:
+    print("2 turtle doves")
+default:
+    print("A partridge in a pear tree")
+}
+
+// Ternary conditional operator
+let canVote = age >= 18 ? "Yes" : "No"
+print(canVote)
+
+let hour = 23
+print(hour < 12 ? "It's before noon" : "It's after noon")
+
+let names = ["Jayne", "Kaylee", "Mal"]
+let crewCount = names.isEmpty ? "No one" : "\(names.count) people"
+print(crewCount)
+
+enum Theme {
+    case light, dark
+}
+
+let theme = Theme.dark
+
+let background = theme == .dark ? "balck" : white
+print(background)
