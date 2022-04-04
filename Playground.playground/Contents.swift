46c9ec68eceda5f189cd045a3e43af0672e71a43
Will <will@goodarc.com>
2022-04-04 15:01:19 +0800

    Day10 - structs, computed properties, and property observers

--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -1,118 +1,114 @@
 import Cocoa
 
-// Closures
-func greetUser() {
-    print("Hi there!")
-}
-
-let sayHello = { (name: String) -> String in
-    "Hi \(name)!"
-}
-
-sayHello("Talyor")
-
-var greetCopy: () -> Void = greetUser
-
-func getUserData(for id: Int) -> String {
-    if id == 1989 {
-        return "TaylorSwift"
-    } else {
-        return "Anonymous"
-    }
-}
-
-let data: (Int) -> String = getUserData
-let user = data(1989)
-print(user)
-
-let team = ["Gloria", "Suzanne", "Piper", "Tinfinny", "Tasha"]
-let sortedTeam = team.sorted()
-print(sortedTeam)
-
-func captainFirstSorted(name1: String, name2: String) -> Bool {
-    if name1 == "Suzanne" {
-        return true
-    } else if name2 == "Suzanne"{
-        return false
-    }
-    return name1 < name2
-}
-
-//let captainFirstTeam = team.sorted(by: captainFirstSorted)
-//print(captainFirstTeam)
-
-let captainFirstTeam = team.sorted(by: { (name1: String, name2: String) -> Bool in
-    if name1 == "Suzanne" {
-        return true
-    } else if name2 == "Suzanne"{
-        return false
-    }
-    return name1 < name2
-})
-
-print(captainFirstTeam)
-
-// Trailing closures and shorthand syntax
-
-let sorted = team.sorted {
-    if $0 == "Suzanne" {
-        return true
-    } else if $1 == "Suzanne"{
-        return false
-    }
-    return $0 < $1
-}
-
-let reverseTeam = team.sorted { $0 > $1 }
-
-let tOnly = team.filter { $0.hasPrefix("T") }
-print(tOnly)
-
-let uppercaseTeam = team.map { $0.uppercased() }
-print(uppercaseTeam)
-
-// Function as parameters
-func makeArray(size: Int, using generator: () -> Int) -> [Int] {
-    var numbers = [Int]()
+// Structs
+struct Album {
+    let title: String
+    let artist: String
+    let year: Int
     
-    for _ in 0..<size {
-        let newNumber = generator()
-        numbers.append(newNumber)
-    }
-    return numbers
-}
-func generateNumber() -> Int {
-    Int.random(in: 1...20)
-}
-let newRolls = makeArray(size: 50, using: generateNumber)
-print(newRolls)
-
-func doImportantWork(first: () -> Void, second: ()-> Void, third: () -> Void) {
-    print("About to start first work")
-    first()
-    print("About to start second work")
-    second()
-    print("About to start third work")
-    third()
-    print("Done!")
-}
-
-doImportantWork {
-    print("This is the first work")
-} second: {
-    print("This is the second work")
-} third: {
-    print("This is the third work")
-}
-
-// Checkpoint 5
-let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
-
-let output = { (luckyNumbers: [Int]) -> Void in
-    let numbers = luckyNumbers.filter { !$0.isMultiple(of: 2) }.sorted { $0 < $1 }.map { "\($0) is a lucky number" }
-    for num in numbers {
-        print(num)
+    func printSummary() {
+        print("\(title) (\(year)) by \(artist)")
     }
 }
 
-output(luckyNumbers)
+let red = Album(title: "Red", artist: "Taylor Swift", year: 2012)
+let wings = Album(title: "Wings", artist: "BTS", year: 2016)
+
+print(red.title)
+print(wings.artist)
+
+red.printSummary()
+wings.printSummary()
+
+struct Employee {
+    let name: String
+    var vacationRemaining: Int = 14
+    
+    mutating func takeVaction(days: Int) {
+        if vacationRemaining > days {
+            vacationRemaining -= days
+            print("I'm going on vacation!")
+            print("Days remaining: \(vacationRemaining)")
+        } else {
+            print("Oops! There aren't enough days remaining.")
+        }
+    }
+}
+
+var archer = Employee(name: "Sterling Archer", vacationRemaining: 14)
+archer.takeVaction(days: 5)
+print(archer.vacationRemaining)
+
+let a = 1
+let b = 2.0
+let c = Double(a) + b
+
+let kane = Employee(name: "lana Kane")
+let poovey = Employee(name: "Pam Poovey", vacationRemaining: 35)
+
+// Compute properties
+struct Employee2 {
+    let name: String
+    var vacationAllocated = 14
+    var vacationTaken = 0
+    
+    var vacationRemaining: Int {
+        get {
+            vacationAllocated - vacationTaken
+        }
+        set {
+            vacationAllocated = vacationTaken + newValue
+        }
+    }
+}
+
+var archer2 = Employee2(name: "Sterling Archer", vacationAllocated: 14)
+archer2.vacationTaken += 4
+archer2.vacationRemaining = 5
+print(archer2.vacationAllocated)
+
+// Porperty observers
+
+struct Game {
+    var score = 0 {
+        didSet {
+            print("Score is now \(score)")
+        }
+    }
+}
+
+var game = Game()
+game.score += 10
+game.score -= 3
+game.score += 1
+    
+struct App {
+    var contacts = [String]() {
+        willSet {
+            print("Current value is : \(contacts)")
+            print("New value will be : \(newValue)")
+        }
+        didSet {
+            print("There are now \(contacts.count) contacts")
+            print("Old value was : \(oldValue)")
+        }
+    }
+}
+
+var app = App()
+app.contacts.append("Adrian E")
+app.contacts.append("Allen W")
+app.contacts.append("Ish S")
+
+// Custom initalizes
+struct Player {
+    let name: String
+    let number: Int
+    init(name: String) {
+        self.name = name
+        number = Int.random(in: 1...99)
+    }
+}
+
+let player = Player(name: "Megan R")
+print(player.number)
