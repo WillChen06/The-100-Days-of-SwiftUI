a5439f4220d1ffeb64f4d301425d0df8551ffd05
Will <will@goodarc.com>
2022-04-28 13:34:03 +0800

    Day40 - SwiftUI - Project 8, part two

---
+++ HundredDaysOfSwiftUI/Project8_MoonShot/Model/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  HundredDaysOfSwiftUI
+//
+//  Created by WillChen on 2022/4/28.
+//
+
+import Foundation
+
+struct Astronaut: Codable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ HundredDaysOfSwiftUI/Project8_MoonShot/Model/Mission.swift
@@ -0,0 +1,34 @@
+//
+//  Mission.swift
+//  HundredDaysOfSwiftUI
+//
+//  Created by WillChen on 2022/4/28.
+//
+
+import Foundation
+
+
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: Date?
+    let crew: [CrewRole]
+    let description: String
+    
+    var displayName: String {
+        "Apollo \(id)"
+    }
+    
+    var image: String {
+        "apollo\(id)"
+    }
+    
+    var formattedLaunchDate: String {
+        launchDate?.formatted(date: .abbreviated, time: .omitted) ?? "N/A"
+    }
+}
