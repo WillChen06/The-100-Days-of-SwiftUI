fa9b11c7365a9f95e9e2a63da8481c5406287eb6
Will <will@goodarc.com>
2022-05-13 14:54:13 +0800

    Day55 - SwiftUI - Project 11, part three

--- HundredDaysOfSwiftUI/Project11_Bookworm/View/BookwormContentView.swift
+++ HundredDaysOfSwiftUI/Project11_Bookworm/View/BookwormContentView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct BookwormContentView: View {
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest(sortDescriptors: []) var books: FetchedResults<Book>
+    @FetchRequest(sortDescriptors: [
+        SortDescriptor(\.title),
+        SortDescriptor(\.author)
+    ]) var books: FetchedResults<Book>
     
     @State private var showingAddScreen = false
     
@@ -18,7 +21,7 @@ struct BookwormContentView: View {
             List {
                 ForEach(books) { book in
                     NavigationLink {
-                        Text(book.title ?? "Unknown Title")
+                        BookDetailView(book: book)
                     } label: {
                         HStack {
                             EmojiRatingView(rating: book.rating)
@@ -33,9 +36,14 @@ struct BookwormContentView: View {
                         }
                     }
                 }
+                .onDelete(perform: deleteBooks)
             }
             .navigationTitle("Bookworm")
             .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                }
+                
                 ToolbarItem(placement: .navigationBarTrailing) {
                     Button {
                         showingAddScreen.toggle()
@@ -48,6 +56,15 @@ struct BookwormContentView: View {
                 AddBookView()
             }
         }
+    }
+    
+    func deleteBooks(at offsets: IndexSet) {
+        for offset in offsets {
+            let book = books[offset]
+            moc.delete(book)
+        }
+        
+        try? moc.save()
     }
 }
 
