77f6282bf02be6cab3a35c061a34386f242869ce
Will <will@goodarc.com>
2022-05-23 17:12:47 +0800

    Day65 - SwiftUI - Project 13, part four

--- HundredDaysOfSwiftUI/Project13_Instafilter/InstafilterContentView.swift
+++ HundredDaysOfSwiftUI/Project13_Instafilter/InstafilterContentView.swift
@@ -5,41 +5,89 @@
 //  Created by WillChen on 2022/5/23.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct InstafilterContentView: View {
     @State private var image: Image?
+    @State private var filterIntensity = 0.5
+    
+    @State private var showingImagePicker = false
     @State private var inputImage: UIImage?
-    @State private var showingImgaePicker = false
+    
+    @State private var currentFilter = CIFilter.sepiaTone()
+    let context = CIContext()
     
     var body: some View {
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
-            
-            Button("Select Image") {
-                showingImgaePicker = true
+        NavigationView {
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .fill(.secondary)
+                    
+                    Text("Tap to select a picture")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                    
+                    image?
+                        .resizable()
+                        .scaledToFit()
+                }
+                .onTapGesture {
+                    showingImagePicker = true
+                }
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                        .onChange(of: filterIntensity) { _ in
+                            applyProcessing()
+                        }
+                }
+                .padding(.vertical)
+                
+                HStack {
+                    Button("Change Filter") {
+                        
+                    }
+                    
+                    Spacer()
+                    
+                    Button("Save", action: save)
+                }
             }
-            
-            Button("Save Image") {
-                guard let inputImage = inputImage else { return }
-
-                let imageSaver = ImageSaver()
-                imageSaver.writeToPhotoAlbum(image: inputImage)
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Instafilter")
+            .onChange(of: inputImage) { _ in
+                loadImage()
             }
-        }
-        .sheet(isPresented: $showingImgaePicker) {
-            ImagePicker(image: $inputImage)
-        }
-        .onChange(of: inputImage) { _ in
-            loadImage()
+            .sheet(isPresented: $showingImagePicker) {
+                ImagePicker(image: $inputImage)
+            }
         }
     }
     
     func loadImage() {
         guard let inputImage = inputImage else { return }
-        image = Image(uiImage: inputImage)
+        let beginImage = CIImage(image: inputImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
+    }
+    
+    func save() {
+        
+    }
+    
+    func applyProcessing() {
+        currentFilter.intensity = Float(filterIntensity)
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
+        }
     }
 }
 
---
+++ HundredDaysOfSwiftUI/Project13_Instafilter/IntergrateUIKitDemo.swift
@@ -0,0 +1,50 @@
+//
+//  IntergrateUIKitDemo.swift
+//  HundredDaysOfSwiftUI
+//
+//  Created by WillChen on 2022/5/23.
+//
+
+import SwiftUI
+
+struct IntergrateUIKitDemo: View {
+    @State private var image: Image?
+    @State private var inputImage: UIImage?
+    @State private var showingImgaePicker = false
+    
+    var body: some View {
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+            
+            Button("Select Image") {
+                showingImgaePicker = true
+            }
+            
+            Button("Save Image") {
+                guard let inputImage = inputImage else { return }
+
+                let imageSaver = ImageSaver()
+                imageSaver.writeToPhotoAlbum(image: inputImage)
+            }
+        }
+        .sheet(isPresented: $showingImgaePicker) {
+            ImagePicker(image: $inputImage)
+        }
+        .onChange(of: inputImage) { _ in
+            loadImage()
+        }
+    }
+    
+    func loadImage() {
+        guard let inputImage = inputImage else { return }
+        image = Image(uiImage: inputImage)
+    }
+}
+
+struct IntergrateUIKitDemo_Previews: PreviewProvider {
+    static var previews: some View {
+        IntergrateUIKitDemo()
+    }
+}
