77240e67a862e67539174f26d0a16fd57d536d23
Will <will@goodarc.com>
2022-06-22 15:03:57 +0800

    Day75 - SwiftUI - Project 15, part two

--- HundredDaysOfSwiftUI/WordScrambleContentView.swift
+++ HundredDaysOfSwiftUI/WordScrambleContentView.swift
@@ -32,6 +32,9 @@ struct WordScrambleContentView: View {
                             Image(systemName: "\(word.count).circle")
                             Text(word)
                         }
+                        .accessibilityElement()
+                        .accessibilityLabel(word)
+                        .accessibilityHint("\(word.count) letters")
                     }
                 }
             }
